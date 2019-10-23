682ce4fcbd787d8c77024d64788f52b549d09686
Jacek Majchrzak <jacek.majchrzak@contractors.roche.com>
2019-10-23 11:11:47 +0200

    write event to file

--- survey/src/main/java/com/eventmodeling/survey/api/FileEventStore.java
+++ survey/src/main/java/com/eventmodeling/survey/api/FileEventStore.java
@@ -2,8 +2,21 @@ package com.eventmodeling.survey.api;
 
 import com.eventmodeling.survey.DomainEvent;
 import com.eventmodeling.survey.EventStore;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 public class FileEventStore implements EventStore {
+
+    public static final String EVENTS_DIRECTORY = "/events";
+
+
+
     @Override
     public DomainEvent load(String aggregateid) {
         throw new UnsupportedOperationException();
@@ -11,6 +24,14 @@ public class FileEventStore implements EventStore {
 
     @Override
     public void save(DomainEvent domainEvent) {
-        throw new UnsupportedOperationException();
+        try {
+            final Path path = Files.createFile(Paths.get(EVENTS_DIRECTORY, String.format("%s-%s", domainEvent.getClass().getSimpleName(),
+                    UUID.randomUUID().toString())));
+            final File file = path.toFile();
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writeValue(file, domainEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
